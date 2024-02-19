0df76cc46525160fe1f32410c0c8d04ba4f9d593
LordOfCheese <94697288+LordOfCheese1@users.noreply.github.com>
2024-02-19 18:30:42 +0100

    why are seeds classified as weapons? I don't know. They just are, take it or leave it.

--- scripts/autoloads/farm_manager.gd
+++ scripts/autoloads/farm_manager.gd
@@ -3,3 +3,16 @@ extends Node2D
 var plant_table = {
 	"carrot" : [load("res://textures/plants/carrot.png"), 3]
 }
+var plants_in_proximity = []
+
+
+func _physics_process(_delta):
+	pass
+
+
+func plant_a_plant(plant_id : String):
+	if len(plants_in_proximity) > 0:
+		var the_plant_in_question = plants_in_proximity[0]
+		the_plant_in_question.stage = 0
+		the_plant_in_question.max_stage = plant_table[plant_id][1]
+		the_plant_in_question.update_visuals()
--- scripts/level_elements/plant.gd
+++ scripts/level_elements/plant.gd
@@ -1,5 +1,24 @@
 extends Area2D
 
+var stage = 0
+var max_stage = 1
+
 
 func _ready():
 	add_to_group("plant")
+
+
+func update_visuals():
+	$sprite.hframes = max_stage
+	$sprite.frame = stage
+
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		FarmManager.plants_in_proximity.insert(0, self)
+
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		if FarmManager.plants_in_proximity.has(self):
+			FarmManager.plants_in_proximity.erase(self)
