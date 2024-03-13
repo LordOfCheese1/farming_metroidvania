13b85865046417a0a2f743f0b7644639cdd467eb
LordOfCheese <94697288+LordOfCheese1@users.noreply.github.com>
2024-03-13 18:05:07 +0100

    (S)atan2

--- scripts/entities/satan_arm.gd
+++ scripts/entities/satan_arm.gd
@@ -8,8 +8,10 @@ var prev_pos = Vector2(0, 0)
 var x_location : float = 0
 var attack_cd = 80
 var is_attacking = 0
+var current_attack = -1
 @export var magic_particle_scene : PackedScene
 @export var magick_proj_scene : PackedScene
+@export var laser_proj : PackedScene
 
 
 func _ready():
@@ -27,13 +29,13 @@ func _physics_process(delta):
 		prev_pos = position
 		position = lerp(position, desirable_pos + Vector2(0, sin(time * 2) * 20), 0.1)
 		
-		x_location = snapped(sin(time * 0.5), 1)
-		desirable_pos.x = in_fight_pos.x + x_location * 500
 		
 		if is_attacking > 0:
-			is_attacking -= 1
 			attack()
+			is_attacking -= 1
 		else:
+			x_location = snapped(sin(time * 0.5), 1)
+			desirable_pos.x = in_fight_pos.x + x_location * 500
 			$visuals.rotation_degrees = lerp($visuals.rotation_degrees, 0.0, 0.1)
 			$visuals/finger2.scale.y = 1.0
 			$visuals/finger3.scale.y = 1.0
@@ -42,6 +44,9 @@ func _physics_process(delta):
 			if attack_cd > 0:
 				attack_cd -= 1
 			else:
+				current_attack += 1
+				if current_attack > 1:
+					current_attack = 0
 				attack_cd = 125
 				is_attacking = 80
 	
@@ -52,27 +57,39 @@ func _physics_process(delta):
 
 
 func attack():
-	var prev_rot = $visuals.rotation_degrees
-	$visuals.look_at(Globals.player_pos + Vector2(0, 1200))
-	$visuals.rotation_degrees -= 90
-	$visuals.rotation_degrees = lerp(prev_rot, $visuals.rotation_degrees, 0.1)
-	$visuals/finger2.scale.y = -0.5
-	$visuals/finger3.scale.y = -0.5
-	$visuals/finger0.rotation_degrees = 0
-	$visuals/finger1.look_at(Globals.player_pos)
-	if is_attacking == 45:
-		ParticleSystem.new_circle(magic_particle_scene, $visuals/finger1.global_position + $visuals/finger1.transform.x * 90, 12, 40)
-	
-	if is_attacking == 18:
-		spawn_projectile()
-	
-	if is_attacking == 10:
-		spawn_projectile()
-	
-	if is_attacking == 2:
-		spawn_projectile()
-	
-	$visuals/finger1.rotation_degrees -= 90
+	if current_attack == 0:
+		var prev_rot = $visuals.rotation_degrees
+		$visuals.look_at(Globals.player_pos + Vector2(0, 1200))
+		$visuals.rotation_degrees -= 90
+		$visuals.rotation_degrees = lerp(prev_rot, $visuals.rotation_degrees, 0.1)
+		$visuals/finger2.scale.y = -0.5
+		$visuals/finger3.scale.y = -0.5
+		$visuals/finger0.rotation_degrees = 0
+		$visuals/finger3.rotation_degrees = 0
+		$visuals/finger1.look_at(Globals.player_pos)
+		if is_attacking == 45:
+			ParticleSystem.new_circle(magic_particle_scene, $visuals/finger1.global_position + $visuals/finger1.transform.x * 90, 12, 40)
+		
+		if is_attacking == 18:
+			spawn_projectile()
+		
+		if is_attacking == 10:
+			spawn_projectile()
+		
+		if is_attacking == 2:
+			spawn_projectile()
+		
+		$visuals/finger1.rotation_degrees -= 90
+	elif current_attack == 1:
+		var prev_rot = $visuals.rotation_degrees
+		desirable_pos.x = lerp(desirable_pos.x, Globals.player_pos.x, 0.05)
+		x_location = 0
+		$visuals.rotation_degrees = lerp(prev_rot, $visuals.rotation_degrees, 0.1)
+		$visuals/finger2.scale.y = -0.5
+		$visuals/finger0.rotation_degrees = -60
+		if is_attacking == 50:
+			spawn_laser(true)
+			spawn_laser(false)
 
 
 func spawn_projectile():
@@ -81,3 +98,14 @@ func spawn_projectile():
 	projectile.position = $visuals/finger1.global_position + $visuals/finger1.transform.x * 90
 	projectile.look_at(Globals.player_pos)
 	get_tree().current_scene.get_node("active_room").get_child(0).get_node("projectiles").call_deferred("add_child", projectile)
+
+
+func spawn_laser(a = false):
+	var laser = laser_proj.instantiate()
+	if a:
+		laser.to_follow = $visuals/finger1/orig
+	else:
+		laser.to_follow = $visuals/finger3/orig
+	laser.target_dir = Vector2(0, 1)
+	laser.lifetime = 50.0 / 60.0
+	get_tree().current_scene.get_node("active_room").get_child(0).get_node("projectiles").call_deferred("add_child", laser)
---
+++ scripts/projectiles/laser.gd
@@ -0,0 +1,34 @@
+extends "res://scripts/base_classes/projectile.gd"
+
+var to_follow = null
+var target_dir = Vector2(1, 0)
+var over = false
+var wanted_alpha = 1.0
+var particle_cd = 0
+
+
+func _ready():
+	setup()
+	modulate.a = 0
+
+
+func _physics_process(delta):
+	projectile_update(delta)
+	
+	if to_follow != null:
+		position = to_follow.global_position
+	
+	$line.width = lerp($line.width, 15 + sin(time * 4) * 4, 0.1)
+	modulate.a = lerp(modulate.a, wanted_alpha, 0.1)
+	$raycast.target_position = target_dir * 2000
+	if $raycast.is_colliding():
+		$line.points[1] = to_local($raycast.get_collision_point())
+		$hurtbox/CollisionShape2D.shape.size.y = position.distance_to($raycast.get_collision_point())
+		$hurtbox/CollisionShape2D.position.y = $hurtbox/CollisionShape2D.shape.size.y * 0.5
+		$hurtbox.look_at($raycast.get_collision_point())
+		$hurtbox.rotation_degrees -= 90
+		if particle_cd > 0:
+			particle_cd -= 1
+		else:
+			ParticleSystem.new_explosion("res://prefabs/particles/particle_melon_juice.tscn", $raycast.get_collision_point(), 12, -PI, PI, -target_dir)
+			particle_cd = 4
